1c6c848017cf6fdc77d5430a8f24f269b4c02315
drewsberry <drew@drewsilcock.co.uk>
2016-11-23 23:04:28 +0000

    Finish first exercise.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 #[cfg(test)]
 mod set1 {
     #[test]
@@ -9,25 +7,31 @@ mod set1 {
 
         let bytes = hex_string_to_bytes(hex_string);
         let base64 = bytes_to_base64(bytes);
+
+        println!("Expected: {}", base64_string);
+        println!("Result:   {}", base64);
+
+        assert!(base64 == base64_string, "Unexpected result.");
     }
 
     fn hex_string_to_bytes(hex_string: &str) -> Vec<u8> {
         let mut bytes_vec = Vec::new();
-        let hex_chars = hex_string.chars();
+        let mut hex_chars = hex_string.chars();
 
         loop {
             let hex_pair = (hex_chars.next(), hex_chars.next());
 
-            match pair {
+            match hex_pair {
                 (Some(first_char), Some(second_char)) => {
                     let first_byte = hex_char_to_byte(first_char);
                     let second_byte = hex_char_to_byte(second_char);
 
                     let combined_byte = first_byte << 4 | second_byte;
                     bytes_vec.push(combined_byte);
-                },
-                (Some(first_char), None) => panic!("Hex strings must come in pairs."),
-                (None, None) => break;
+                }
+                (Some(_), None) => panic!("Hex strings must come in pairs."),
+                (None, None) => break,
+                _ => panic!(),
             }
         }
 
@@ -36,27 +40,68 @@ mod set1 {
 
     fn hex_char_to_byte(hex_char: char) -> u8 {
         match hex_char {
-            '0' .. '9' => return hex_char as u8 - '0' as u8 + 0x00,
-            'a' .. 'z' => return hex_char as u8 - 'a' as u8 + 0x0a,
-            'A' .. 'Z' => return hex_char as u8 - 'A' as u8 + 0x0a,
+            '0'...'9' => return hex_char as u8 - '0' as u8 + 0x00,
+            'a'...'z' => return hex_char as u8 - 'a' as u8 + 0x0a,
+            'A'...'Z' => return hex_char as u8 - 'A' as u8 + 0x0a,
             _ => panic!("Character {} is not valid hex.", hex_char),
         }
     }
 
-    const BASE_64: [char; 64] = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'a', 'b', 'c',
-        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
-        '5', '6', '7', '8', '9', '+', '/',
-    ];
+    const BASE_64: [char; 64] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
 
-    fn bytes_to_base64(bytes: Vec<u8>) -> String {
+    fn bytes_to_base64(mut bytes: Vec<u8>) -> String {
         let mut base64 = String::new();
+        bytes.reverse();
 
         loop {
-            let quadruple = ()
-            base64.push(new_char);
+            match (bytes.pop(), bytes.pop(), bytes.pop()) {
+                (Some(first), Some(second), Some(third)) => {
+                    let new_chars = byte_triplet_to_base64_quadruplet(first, second, third);
+                    for new_char in new_chars.iter() {
+                        base64.push(*new_char);
+                    }
+                }
+                (Some(first), Some(second), None) => {
+                    println!("Two instead of three...");
+                    let new_chars = byte_triplet_to_base64_quadruplet(first, second, 0);
+                    for new_char in new_chars.iter() {
+                        base64.push(*new_char);
+                    }
+                    break;
+                }
+                (Some(first), None, None) => {
+                    println!("One instead of three...");
+                    let new_chars = byte_triplet_to_base64_quadruplet(first, 0, 0);
+                    for new_char in new_chars.iter() {
+                        base64.push(*new_char);
+                    }
+                    break;
+                }
+                (None, None, None) => {
+                    break;
+                }
+                _ => panic!(),
+            }
         }
+
+        base64
+    }
+
+    fn byte_triplet_to_base64_quadruplet(first: u8, second: u8, third: u8) -> [char; 4] {
+        let byte_quadruplet = [first >> 2,
+                               (first & 0b00_00_00_11) << 4 | second >> 4,
+                               (second & 0b00_00_11_11) << 2 | third >> 6,
+                               third & 0b00_11_11_11];
+
+        let base64_quadruplet = [BASE_64[byte_quadruplet[0] as usize],
+                                 BASE_64[byte_quadruplet[1] as usize],
+                                 BASE_64[byte_quadruplet[2] as usize],
+                                 BASE_64[byte_quadruplet[3] as usize]];
+
+        base64_quadruplet
     }
 }
