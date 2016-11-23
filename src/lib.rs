ee05763c96e4789820a9c1d8c982d4aafaf16593
drewsberry <drew@drewsilcock.co.uk>
2016-11-23 23:39:26 +0000

    Complete second challenge.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,30 @@ mod set1 {
         assert!(base64 == base64_string, "Unexpected result.");
     }
 
+    // Target: XOR two hex strings.
+    #[test]
+    fn challenge2() {
+        let first_hex = "1c0111001f010100061a024b53535009181c";
+        let second_hex = "686974207468652062756c6c277320657965";
+        let expected_result = "746865206b696420646f6e277420706c6179";
+
+        let first_bytes = hex_string_to_bytes(first_hex);
+        let second_bytes = hex_string_to_bytes(second_hex);
+
+        let xor_result: Vec<u8> = first_bytes
+            .iter()
+            .zip(second_bytes.iter())
+            .map(|byte_pair| byte_pair.0 ^ byte_pair.1)
+            .collect();
+
+        let xor_string = bytes_to_hex_string(xor_result);
+
+        println!("Expected: {}", expected_result);
+        println!("Result:   {}", xor_string);
+
+        assert!(xor_string == expected_result, "Unexpected result.");
+    }
+
     fn hex_string_to_bytes(hex_string: &str) -> Vec<u8> {
         let mut bytes_vec = Vec::new();
         let mut hex_chars = hex_string.chars();
@@ -38,12 +62,42 @@ mod set1 {
         bytes_vec
     }
 
+    fn bytes_to_hex_string(bytes: Vec<u8>) -> String {
+        let mut hex_string = String::new();
+
+        for byte in bytes {
+            let (first_char, second_char) = byte_to_hex_tuple(byte);
+            hex_string.push(first_char);
+            hex_string.push(second_char);
+        }
+
+        hex_string
+    }
+
     fn hex_char_to_byte(hex_char: char) -> u8 {
         match hex_char {
             '0'...'9' => return hex_char as u8 - '0' as u8 + 0x00,
             'a'...'z' => return hex_char as u8 - 'a' as u8 + 0x0a,
             'A'...'Z' => return hex_char as u8 - 'A' as u8 + 0x0a,
             _ => panic!("Character {} is not valid hex.", hex_char),
+        }
+    }
+
+    fn byte_to_hex_tuple(byte: u8) -> (char, char) {
+        let first_byte = byte >> 4;
+        let second_byte = byte & 0x0f;
+
+        let first_char = half_byte_to_hex_char(first_byte);
+        let second_char = half_byte_to_hex_char(second_byte);
+
+        (first_char, second_char)
+    }
+
+    fn half_byte_to_hex_char(half_byte: u8) -> char {
+        match half_byte {
+            0x0...0x9 => return (half_byte - 0x0 + '0' as u8) as char,
+            0xa...0xf => return (half_byte - 0xa + 'a' as u8) as char,
+            _ => panic!(),
         }
     }
 
